bfa954203de82ac36c4acd0bf3c38655e8205bb1
tesskl <tessie_bell@hotmail.com>
2016-03-30 11:35:54 +0200

    files fixed

--- app/src/main/java/com/example/therese/tessapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/therese/tessapp/DisplayMessageActivity.java
@@ -23,9 +23,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null)
-                            .show();
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
                 }
             });
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
